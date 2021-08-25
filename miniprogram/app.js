40e52128d40ae21d5ca23ab6f6bb15bee0c4a773
Sheldon <1823390946@qq.com>
2021-08-25 14:15:01 +0800

    补充之前上传内容

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,11 +1,13 @@
-//注册小程序应用
 App({
 
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function () {
-    
+    //云开发环境初始化
+    wx.cloud.init({
+      env:"cloud1-7gh3ock7a08defbd"
+    })
   },
 
   /**
